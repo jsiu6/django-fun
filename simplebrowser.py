c46f1826ac4edcb05b0614656a666b56861fe01b
James <james@Ken-2.local>
2021-08-01 19:24:34 -0700

    Supported Dynamic URL

--- simplebrowser.py
+++ simplebrowser.py
@@ -1,14 +1,18 @@
 import socket
 
 info = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-info.connect(('data.pr4e.org', 80))
-command = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
-info.send(command)
+try:
+    link = input('Enter link: ')
+    info.connect((link, 80))
 
-while True:
-    data = info.recv(512)
-    if len(data) < 1:
-        break
-    print(data.decode(), end='')
+    command = 'GET ' + link + ' HTTP/1.0\r\n\r\n'
+    info.send(command.encode())
 
+    while True:
+        data = info.recv(512)
+        if len(data) < 1:
+            break
+        print(data.decode(), end='')
+except Exception as ex:
+    print('Error: ' + str(ex))
 info.close()
