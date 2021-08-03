5deb4851728dc9e572cd24cea5743eebf1ceb2d5
James <james@Ken-2.local>
2021-08-03 12:38:46 -0700

    Updated homepage

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Insert your text here.")
+    return HttpResponse("Welcome to my website. I hope my code helped you!")
