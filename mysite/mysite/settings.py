d4742f27a7eb0658f11d92739021b6dc3894af7e
James <james@Ken-2.local>
2021-09-06 14:03:40 -0700

    Links to web pages and using cookies + sessions, tracking views

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'activities.apps.ActivitiesConfig',
     'polls.apps.PollsConfig',
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
